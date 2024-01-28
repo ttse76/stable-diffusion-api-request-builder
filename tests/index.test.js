0bee4901702bb8c8cbe450a4c40bfea7b4aa4dac
Tim Tse <timtse76@gmail.com>
2024-01-28 04:23:30 -0500

    added tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,12 +1,28 @@
 const StableDiffusionRequestBuilder = require('../StableDiffusionRequestBuilder');
 
 test('builds a request with the given parameters', () => {
-  const testPrompt = 'my test prompt';
-  const testSeed = 1234;
+  const testRequestBase = {
+    prompt: 'my test prompt',
+    negative_prompt: 'my negative prompt',
+    seed: 1234,
+    sampler_index: 'test sampler',
+    steps: 20,
+    cfg_scale: 7
+  }
 
   const testRequest = new StableDiffusionRequestBuilder()
-    .setPrompt(testPrompt)
-    .setSeed(testSeed);
+    .setPrompt(testRequestBase.prompt)
+    .setNegativePrompt(testRequestBase.negative_prompt)
+    .setSeed(testRequestBase.seed)
+    .setSamplerIndex(testRequestBase.sampler_index)
+    .setSteps(testRequestBase.steps)
+    .setCfgScale(testRequestBase.cfg_scale);
 
-  expect(testRequest).toEqual({ prompt: testPrompt, seed: testSeed });
+  expect(testRequest).toEqual(testRequestBase);
+});
+
+test('builds a request with default parameters if no parameters are added', () => {
+  const testRequest = new StableDiffusionRequestBuilder();
+
+  expect(testRequest).toEqual({ prompt: '', seed: -1 });
 });
